d96f0df19f363a2ed597ada7c85cbca15198731b
hippogyz <77163339+hippogyz@users.noreply.github.com>
2025-05-27 00:38:40 +0800

    prepare for wash fill stage

--- src/components/wash.gd
+++ src/components/wash.gd
@@ -7,6 +7,13 @@ class_name Wash extends Resource
 @export_range(0.0, 1.0) var edge_max : float = 0.9
 @export var edge_pow : float = 1.0
 
+@export_group("Fill")
+@export_range(0.0, 1.0) var fill_strength : float = 0.1
+@export var fill_uv_scale : Vector2 = Vector2.ONE
+@export var fill_uv_offset : Vector2 = Vector2.ZERO
+@export var fill_uv_offset_shift : Vector2 = Vector2.ONE * 0.3
+@export var fill_uv_offset_rot : float = 0.3
+
 
 func override(source : Wash) -> void:
 	self.enable = source.enable
